aa88431fb5e9443a2dcb88eaec7f991ef28ce382
christaupher <christopher.ly@epita.fr>
2018-12-14 20:21:51 +0100

    Adding one more player in the cooperation

--- src/Game/PlayerIA/ChristaupherPlayer.php
+++ src/Game/PlayerIA/ChristaupherPlayer.php
@@ -44,7 +44,7 @@ class ChristaupherPlayer extends Player
     {
 
         // part of the dream team, we will help each other
-        $dream_team = array('PacoTheGreat', 'FelixDupriez', 'Shiinsekai', 'Ghope', 'Etienneelg', 'Christaupher');
+        $dream_team = array('PacoTheGreat', 'FelixDupriez', 'Shiinsekai', 'Ghope', 'Etienneelg', 'Benli06');
 
         $oppName = $this->result->getStatsFor($this->opponentSide)['name'];
                 if (in_array($oppName, $dream_team))
